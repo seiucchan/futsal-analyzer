f72afa28dd7958c6a5a19302600695ee1559b567
Omura Shohei <seiucchan@gmail.com>
2020-04-25 17:34:57 +0900

    split module into other files

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-from yolo import *
+from modules.yolo import *
+from modules.sort import *
+from modules.functions import *
 from utils import *
 import os, sys, time, datetime, random
 sys.path.append(os.pardir)
@@ -11,7 +13,8 @@ from torch.autograd import Variable
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from PIL import Image
-from functions import YOLO, draw_bbox
+import cv2
+from IPython.display import clear_output
 
 
 config_path='../config/yolov3.cfg'
@@ -29,89 +32,12 @@ classes = utils.load_classes(class_path)
 # Tensor = torch.cuda.FloatTensor
 Tensor = torch.Tensor
 
-def revert_coord(coord: tuple, base: int =416,
-                  imsize: tuple =(1280, 720)) -> tuple:
-    new_x = coord[0] * imsize[0] // base
-    new_y = coord[1] * imsize[1] // base
-    return (new_x, new_y)
-
-def detect_image(img):
-    # scale and pad image
-    ratio = min(img_size/img.size[0], img_size/img.size[1])
-    imw = round(img.size[0] * ratio)
-    imh = round(img.size[1] * ratio)
-    print("h-w:",max(int((imh-imw)/2),0)) #->0
-    print("w-h:", max(int((imw-imh)/2),0)) #->91
-    img_transforms=transforms.Compose([transforms.Resize((imh,imw)),
-         transforms.Pad((max(int((imh-imw)/2),0),
-              max(int((imw-imh)/2),0), max(int((imh-imw)/2),0),
-              max(int((imw-imh)/2),0)), (128,128,128)),
-         transforms.ToTensor(),
-         ])
-    # convert image to Tensor
-    image_tensor = img_transforms(img).float()
-    image_tensor = image_tensor.unsqueeze_(0)
-    input_img = Variable(image_tensor.type(Tensor))
-    print("input_img:", input_img.shape)
-    # run inference on the model and get detections
-    with torch.no_grad():
-        detections = model(input_img)
-        detections = utils.non_max_suppression(detections, 0.5, 0.4)
-    return detections[0]
-
-class PointList():
-    def __init__(self, npoints):
-        self.npoints = npoints
-        self.ptlist = np.empty((npoints, 2), dtype=int)
-        self.pos = 0
-
-    def add(self, x, y):
-        if self.pos < self.npoints:
-            self.ptlist[self.pos, :] = [x, y]
-            self.pos += 1
-            return True
-        return False
-
-
-def onMouse(event, x, y, flag, params):
-    wname, img, ptlist = params
-    if event == cv2.EVENT_MOUSEMOVE:  # マウスが移動したときにx線とy線を更新する
-        img2 = np.copy(img)
-        h, w = img2.shape[0], img2.shape[1]
-        cv2.line(img2, (x, 0), (x, h - 1), (255, 0, 0))
-        cv2.line(img2, (0, y), (w - 1, y), (255, 0, 0))
-        cv2.imshow(wname, img2)
-
-    if event == cv2.EVENT_LBUTTONDOWN:  # レフトボタンをクリックしたとき、ptlist配列にx,y座標を格納する
-        if ptlist.add(x, y):
-            print('[%d] ( %d, %d )' % (ptlist.pos - 1, x, y))
-            cv2.circle(img, (x, y), 3, (0, 0, 255), 3)
-            cv2.imshow(wname, img)
-        else:
-            print('All points have selected.  Press ESC-key.')
-        if(ptlist.pos == ptlist.npoints):
-            print(ptlist.ptlist)
-            cv2.line(img, (ptlist.ptlist[0][0], ptlist.ptlist[0][1]),
-                     (ptlist.ptlist[1][0], ptlist.ptlist[1][1]), (0, 255, 0), 3)
-            cv2.line(img, (ptlist.ptlist[1][0], ptlist.ptlist[1][1]),
-                     (ptlist.ptlist[2][0], ptlist.ptlist[2][1]), (0, 255, 0), 3)
-            cv2.line(img, (ptlist.ptlist[2][0], ptlist.ptlist[2][1]),
-                     (ptlist.ptlist[3][0], ptlist.ptlist[3][1]), (0, 255, 0), 3)
-            cv2.line(img, (ptlist.ptlist[3][0], ptlist.ptlist[3][1]),
-                     (ptlist.ptlist[4][0], ptlist.ptlist[4][1]), (0, 255, 0), 3)
-            cv2.line(img, (ptlist.ptlist[4][0], ptlist.ptlist[4][1]),
-                     (ptlist.ptlist[0][0], ptlist.ptlist[0][1]), (0, 255, 0), 3)
-
-
 
 videopath = '../seiucchanvideo.mp4'
 
-import cv2
-from IPython.display import clear_output
 cmap = plt.get_cmap('tab20b')
 colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
-# initialize Sort object and video capture
-from sort import *
+
 vid = cv2.VideoCapture(videopath)
 ret, frame = vid.read()
 img = frame
@@ -134,7 +60,7 @@ while(True):
             sys.exit(0)
 
         pilimg = Image.fromarray(frame)
-        detections = detect_image(pilimg)
+        detections = detect_image(pilimg, img_size, Tensor, model)
         out_box_indices = []
         detections_ = np.array(detections)
         detections = list()

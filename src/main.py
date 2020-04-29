c7ee6bb911f57827ce18452b99eaade2a5ebd44d
Omura Shohei <seiucchan@gmail.com>
2020-04-29 18:01:45 +0900

    split into function

--- src/main.py
+++ src/main.py
@@ -26,6 +26,7 @@ import click
 @click.option('--conf_thres', default=0.8)
 @click.option('--nms_thres', default=0.4)
 @click.option('--videopath', default='seiucchanvideo.mp4')
+@click.option('--npoints', default=5)
 
 def main(config_path,
          weights_path,
@@ -33,10 +34,11 @@ def main(config_path,
          img_size,
          conf_thres,
          nms_thres,
-         videopath):
+         videopath,
+         npoints):
 
     # Load model and weights
-    model = Darknet(config_path, img_size=416).to("cpu")
+    model = Darknet(config_path, img_size).to("cpu")
     model.load_darknet_weights(weights_path)
     # model.cuda()
     model.eval()
@@ -45,15 +47,12 @@ def main(config_path,
     Tensor = torch.Tensor
 
 
-    cmap = plt.get_cmap('tab20b')
-    colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
 
     vid = cv2.VideoCapture(videopath)
     ret, frame = vid.read()
     img = frame
     wname = "MouseEvent"
     cv2.namedWindow(wname)
-    npoints = 5
     ptlist = PointList(npoints)
     cv2.setMouseCallback(wname, onMouse, [wname, img, ptlist])
     cv2.waitKey()
@@ -66,67 +65,19 @@ def main(config_path,
             ret, frame = vid.read()
 
             if type(frame) == type(None):
-                print('ratio:', np.mean(count_boxes) / 10)
+                # print('ratio:', np.mean(count_boxes) / 10)
                 sys.exit(0)
 
             pilimg = Image.fromarray(frame)
             detections = detect_image(pilimg, img_size, Tensor, model)
-            out_box_indices = []
             detections_ = np.array(detections)
-            detections = list()
-            person_id = 0.0
-            for det in detections_:
-                if det[6] == person_id:
-                    detections.append(det)
-            detections = np.array(detections)
-
-            img = np.array(pilimg)
-            pad_x = max(img.shape[0] - img.shape[1], 0)*(img_size / max(img.shape))
-            pad_y = max(img.shape[1] - img.shape[0], 0)*(img_size / max(img.shape))
-            unpad_h = img_size - pad_y
-            unpad_w = img_size - pad_x
-
-            for i, coord in enumerate(detections):
-                x1, y1, x2, y2 = coord[:4]
-                box_h = int(((y2 - y1) / unpad_h) * img.shape[0])
-                box_w = int(((x2 - x1) / unpad_w) * img.shape[1])
-                y1 = int(((y1 - pad_y // 2) / unpad_h) * img.shape[0])
-                x1 = int(((x1 - pad_x // 2) / unpad_w) * img.shape[1])
-                y2 = y1 + box_h
-                x2 = x1 + box_w
-                pt = ((x1 + x2) // 2, y2 - (box_h // 5))
-                # pt = revert_coord(pt)
-
-                result =  cv2.pointPolygonTest(ptlist.ptlist, pt, False)
-                if result < 0:
-                    out_box_indices.insert(0, i)
-                # else:
-                #     print('---', x1, y1, x2, y2)
-            # sys.exit(0)
-            for i in out_box_indices:
-                # sys.exit(0)
-                detections = np.delete(detections, i, 0)
-
-            count_boxes.append(len(detections))
-
+            detections = filter_coat(detections_, pilimg, img_size, ptlist)
             detections = torch.tensor(detections)
 
             if detections is not None:
                 tracked_objects = mot_tracker.update(detections.cpu())
-                # unique_labels = detections[:, -1].cpu().unique()
-                # n_cls_preds = len(unique_labels)
-                for x1, y1, x2, y2, obj_id in tracked_objects:
-                    box_h = int(((y2 - y1) / unpad_h) * img.shape[0])
-                    box_w = int(((x2 - x1) / unpad_w) * img.shape[1])
-                    y1 = int(((y1 - pad_y // 2) / unpad_h) * img.shape[0])
-                    x1 = int(((x1 - pad_x // 2) / unpad_w) * img.shape[1])
-                    color = colors[int(obj_id) % len(colors)]
-                    color = [i * 255 for i in color]
-                    cls = classes[int(0)]
-                    cv2.rectangle(frame, (x1, y1), (x1+box_w, y1+box_h), color, 4)
-                    # print('===', x1, y1, x1+box_w, y1+box_h)
-            cv2.imshow("mot_tracker", frame)
-            key = cv2.waitKey(1) & 0xFF
+                visualization(tracked_objects, pilimg, img_size, img, classes, frame)
+
 
 if __name__ == '__main__':
     main()
--- src/modules/functions.py
+++ src/modules/functions.py
@@ -84,3 +84,65 @@ def onMouse(event, x, y, flag, params):
                      (ptlist.ptlist[4][0], ptlist.ptlist[4][1]), (0, 255, 0), 3)
             cv2.line(img, (ptlist.ptlist[4][0], ptlist.ptlist[4][1]),
                      (ptlist.ptlist[0][0], ptlist.ptlist[0][1]), (0, 255, 0), 3)
+
+def filter_coat(detections_, pilimg, img_size, ptlist):
+    #exclude result of detection exceot person
+    out_box_indices = []
+    detections = list()
+    person_id = 0.0
+    for det in detections_:
+        if det[6] == person_id:
+            detections.append(det)
+    detections = np.array(detections)
+
+    pad_x, pad_y, unpad_h, unpad_w = change_coord(pilimg, img_size)
+    # chose out_box_indices
+    img = np.array(pilimg)
+
+    for i, coord in enumerate(detections):
+        x1, y1, x2, y2 = coord[:4]
+        box_h = int(((y2 - y1) / unpad_h) * img.shape[0])
+        box_w = int(((x2 - x1) / unpad_w) * img.shape[1])
+        y1 = int(((y1 - pad_y // 2) / unpad_h) * img.shape[0])
+        x1 = int(((x1 - pad_x // 2) / unpad_w) * img.shape[1])
+        y2 = y1 + box_h
+        x2 = x1 + box_w
+        pt = ((x1 + x2) // 2, y2 - (box_h // 5))
+
+        result =  cv2.pointPolygonTest(ptlist.ptlist, pt, False)
+        if result < 0:
+            out_box_indices.insert(0, i)
+
+    # delete out_box
+    for i in out_box_indices:
+        detections = np.delete(detections, i, 0)
+
+    # count_boxes.append(len(detections))
+    return detections
+
+
+def change_coord(pilimg, img_size):
+    img = np.array(pilimg)
+    pad_x = max(img.shape[0] - img.shape[1], 0)*(img_size / max(img.shape))
+    pad_y = max(img.shape[1] - img.shape[0], 0)*(img_size / max(img.shape))
+    unpad_h = img_size - pad_y
+    unpad_w = img_size - pad_x
+    return pad_x, pad_y, unpad_h, unpad_w
+
+def visualization(tracked_objects, pilimg, img_size, img, classes, frame):
+    cmap = plt.get_cmap('tab20b')
+    colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
+
+    pad_x, pad_y, unpad_h, unpad_w = change_coord(pilimg, img_size)
+    for x1, y1, x2, y2, obj_id in tracked_objects:
+        box_h = int(((y2 - y1) / unpad_h) * img.shape[0])
+        box_w = int(((x2 - x1) / unpad_w) * img.shape[1])
+        y1 = int(((y1 - pad_y // 2) / unpad_h) * img.shape[0])
+        x1 = int(((x1 - pad_x // 2) / unpad_w) * img.shape[1])
+        color = colors[int(obj_id) % len(colors)]
+        color = [i * 255 for i in color]
+        cls = classes[int(0)]
+        cv2.rectangle(frame, (x1, y1), (x1+box_w, y1+box_h), color, 4)
+    cv2.imshow("mot_tracker", frame)
+    key = cv2.waitKey(1) & 0xFF
+    return key

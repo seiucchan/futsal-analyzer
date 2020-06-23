0f2fb29c7c2b4ed642fb08ed293ffd1d45cbdea7
Omura Shohei <seiucchan@gmail.com>
2020-06-23 15:48:50 +0900

    fix conflict

--- src/analyzer/utils/visualization.py
+++ src/analyzer/utils/visualization.py
@@ -5,18 +5,6 @@ from PIL import Image
 from utils.change_coord import change_coord
 
 
-<<<<<<< HEAD:src/utils/visualization.py
-def visualization(tracked_objects, pilimg, img_size, img, classes, frame, preds):
-    cmap = plt.get_cmap('tab20b')
-    colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
-    font = cv2.FONT_HERSHEY_SIMPLEX
-
-    pad_x, pad_y, unpad_h, unpad_w = change_coord(pilimg, img_size)
-
-    i = 0
-
-    for x1, y1, x2, y2, obj_id in tracked_objects:
-=======
 def visualization(tracked_objects, pilimg, img_size, img, classes, frame, is_show):
     cmap = plt.get_cmap('tab20b')
     # colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
@@ -24,7 +12,6 @@ def visualization(tracked_objects, pilimg, img_size, img, classes, frame, is_sho
     pad_x, pad_y, unpad_h, unpad_w = change_coord(pilimg, img_size)
     # for x1, y1, x2, y2, obj_id in tracked_objects:
     for x1, y1, x2, y2, _, _, _ in tracked_objects:
->>>>>>> 3be281f87b42091529539b751221bb0f5bc16e3b:src/analyzer/utils/visualization.py
         box_h = int(((y2 - y1) / unpad_h) * img.shape[0])
         box_w = int(((x2 - x1) / unpad_w) * img.shape[1])
         y1 = int(((y1 - pad_y // 2) / unpad_h) * img.shape[0])
@@ -34,7 +21,6 @@ def visualization(tracked_objects, pilimg, img_size, img, classes, frame, is_sho
         color = [0, 255, 255]
         cls = classes[int(0)]
         cv2.rectangle(frame, (x1, y1), (x1+box_w, y1+box_h), color, 4)
-<<<<<<< HEAD:src/utils/visualization.py
         # pred_id = obj_id - 1
         # pred_id = int(pred_id)
         print(preds, i)
@@ -49,9 +35,8 @@ def visualization(tracked_objects, pilimg, img_size, img, classes, frame, is_sho
 
         i += 1
     cv2.imshow("mot_tracker", frame)
-=======
+    
     if is_show:
         cv2.imshow("mot_tracker", frame)
->>>>>>> 3be281f87b42091529539b751221bb0f5bc16e3b:src/analyzer/utils/visualization.py
     key = cv2.waitKey(1) & 0xFF
     return frame
